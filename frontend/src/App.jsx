6ccdc7445bce20a1616abc41aaec4683bc3a46b0
Adrian <adriancabellomartin2003@gmail.com>
2025-02-08 23:50:05 +0100

    LIMPIEZA PLANTILLA "

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontSize: '24px' }}>
+      <h1>Bienvenido a Group and Play 🎉</h1>
+      <p>La mejor plataforma para conectar grupos de juego y eventos</p>
+    </div>
+  );
+}
+
+export default App;
+
